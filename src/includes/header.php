c72afdd08a2e33e07f93b1b25be51fdb445504f3
Hazar Gul NazarI <hazar.nazari@check24.de>
2025-03-22 20:28:29 +0100

    refactor: remove about section with project contributors

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,31 +4,7 @@
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
                     <h4>About</h4>
-                    <!-- bring changes -->
-                    <p class="text-muted">Developers Community is an application for connecting develoers all around the world, it also let the developers to generate the UML diagrams from their PHP 5.0 source code</p>
-                    <div class="container marketing">
-
-                <div class="row text-center">
-                  <div class="col-lg-4">
-                    <img class="rounded-circle" src="resume/medesaturated.jpg" alt="Azargul Nazari Picture" width="140" height="140">
-                    <p><b>Hazar Gul Nazari</b></p>
-                    <p><small>Undergradate Student in King Abdulaziz University and this senior project is developed by Hazargul Nazari</small></p>
-                  </div><!-- /.col-lg-4 -->
-                  <div class="col-lg-4">
-                    <img class="rounded-circle" src="resume/drfathydesaturated.png" alt="Professor Fathy Picture" width="140" height="140">
-                    <p><b>Prof. Fathy Eassa</b></p>
-                    <p>Professor Fathy Eassa is the main supervisor for this senior project</p>
-                    <p><a class="btn btn-success" href="resume/profFathy.PDF" role="button">Know More</a></p>
-                  </div><!-- /.col-lg-4 -->
-                  <div class="col-lg-4">
-                    <img class="rounded-circle" src="resume/drkamaldesaturated.png" alt="Professor Kamal Jambi Picture" width="140" height="140">
-                    <p><b>Prof. Kamal Jambi</b></p>
-                    <p>Professor Jambi is the co-supervisor for this senior project</p>
-                    <p><a class="btn btn-success" href="resume/profKamal.PDF" role="button">Know More</a></p>
-                  </div><!-- /.col-lg-4 -->
-                </div><!-- /.row -->
-
-      </div>
+                    <p class="text-muted">Developers Community is an application for connecting developers all around the world, it also let the developers to generate the UML diagrams from their PHP 5.0 source code</p>
                 </div>
             </div>
         </div>
